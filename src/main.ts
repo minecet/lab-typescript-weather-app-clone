f550f92799386bef12caec6126a50d02f344d656
minecet <mine.cetinkaya@gmail.com>
2025-02-06 18:52:18 +0100

    solved

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,98 @@
 // src/main.ts
+import { getLocation, getCurrentWeather, displayLocation, displayWeatherData } from './utils.ts';
+
+const form = document.getElementById("weather-form") as HTMLFormElement;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const locationInput = document.getElementById("location") as HTMLInputElement;
+    const locationName = locationInput.value;
+    
+    //console.log(`The user has submitted the form and is searching for a location with this name... ${locationName} `);
+  
+    locationInput.value = ""; // Clear the form
+    getLocation(locationName)
+    .then((response) => {
+      if(response.results){
+
+        // Get the first result (the api may provide multiple results if there's multiple locations with the same or similar names, we will just use the first one for simplicity)
+        const location = response.results[0];
+
+        // Display info about the location
+        displayLocation(location);
+
+        // Get info about the weather for that location
+        return getCurrentWeather(location);
+      } else {
+        // If there's no results, throw an error
+        throw new Error('Location not found');
+      }
+    })
+    .then((weatherData) => {
+
+      // Display info about the weather
+      displayWeatherData(weatherData);
+      updateBackground(weatherData.current_weather.weathercode, weatherData.current_weather.is_day);  
+    })
+    .catch((error) => {
+      console.log("Error getting weather data");
+      console.log(error);
+    });
+});
+
+function updateBackground(weatherCode: number, isDay: number): void {
+    switch(weatherCode) {
+        case 0: 
+        case 1:
+            if(isDay === 0){
+                document.body.className = "sunny-night"
+            } else if(isDay === 1){
+                document.body.className = "sunny"
+            }
+            break;
+        case 2: 
+        if(isDay === 0){
+            document.body.className = "partly-cloudy-night"
+        } else if(isDay === 1){
+            document.body.className = "partly-cloudy"
+        }        
+            break;
+
+        case 3:
+            document.body.className = "cloudy"
+
+            break;
+
+        case 4:
+            document.body.className = "foggy"
+
+            break;
+
+        case 5:
+            document.body.className = "drizzle"
+
+            break;
+
+        case 6:
+            document.body.className = "rain"
+
+            break;
+
+        case 7:
+            document.body.className = "snow"
+
+            break;
+
+        case 8:
+            document.body.className = "showers"
+
+            break;
+
+        case 9:
+            document.body.className = "thunderstorm"
+
+            break;
+
+    }
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 // src/utils.ts
 
 import axios from 'axios';
-import { LocationResponse, Location } from "./types";
+import { LocationResponse, Location, WeatherResponse } from "./types";
 
 
 
@@ -12,3 +12,26 @@ export function getLocation(locationName: string): Promise<LocationResponse> {
 
 
 
+export async function getCurrentWeather(locationDetails: Location): Promise<WeatherResponse>{
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`
+    return axios.get(url).then((response) => response.data);
+
+}
+
+export function displayLocation(locationDetails: Location){
+    const locationName = document.getElementById('location-name') as HTMLElement;
+    locationName.innerText = locationDetails.name;
+    const country = document.getElementById('country') as HTMLElement;
+    country.innerText = locationDetails.country;
+
+}
+
+export function displayWeatherData(obj: WeatherResponse): void{
+    const temp = document.getElementById('temperature') as HTMLElement;
+    temp.innerText = obj.current_weather.temperature + " " + obj.current_weather_units.temperature
+    const windspeed = document.getElementById('windspeed') as HTMLElement;
+    windspeed.innerText = obj.current_weather.windspeed + " " + obj.current_weather_units.windspeed
+    const winddirection = document.getElementById('winddirection') as HTMLElement;
+    winddirection.innerText = obj.current_weather.winddirection + " " + obj.current_weather_units.winddirection
+
+}
